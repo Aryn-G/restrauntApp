3c041d99f91147937db3b13f0f31d5e7153b0029
Aryan Gera <aryangera@hotmail.com>
2021-07-14 13:21:43 -0400

    save

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const tables = [];
+const waitlist = [];
+
+const menu = [
+  {
+    food: "pizza",
+    price: 2.00,
+  },
+  {
+    food: "fries",
+    price: 0.50,
+  },
+  {
+    food: "sprite",
+    price: 0.50,
+  },
+  {
+    food: "chips",
+    price: 0.50,
+  },
+];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/order", function (req, res) {
+  res.sendFile(path.join(__dirname, "order.html"));
+});
+
+app.get("/api/tables", function (req, res) {
+  res.json(tables);
+});
+
+app.get("/api/waitlist", function (req, res) {
+  res.json(waitlist);
+});
+
+app.get("/api/menu", function (req, res) {
+  res.json(waitlist);
+});
+
+app.get("/book", function (req, res) {
+  res.sendFile(path.join(__dirname, "temp.html"));
+});
+
+app.post("/api/tables", function (req, res) {
+  var newTable = req.body;
+
+  console.log(newTable);
+
+  tables.push(newTable);
+
+  res.json(newTable);
+});
+
+app.post("/api/waitlist", function (req, res) {
+  var newWait = req.body;
+
+  console.log(newWait);
+
+  waitlist.push(newWait);
+
+  res.json(newWait);
+});
+
+app.listen(PORT, function () {
+  console.log("Listening at http://localhost:" + PORT);
+});
